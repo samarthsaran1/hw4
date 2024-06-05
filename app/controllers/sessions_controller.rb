5d1b78ae74f4f7a41dbf5fabe63306f4908f120c
Samarth Saran <samarth.saran@kellogg.northwestern.edu>
2024-06-05 07:48:39 +0000

    commit 5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,16 @@ class SessionsController < ApplicationController
     @user = User.find_by({"email"=> params["email"]})
     if @user != nil
       if BCrypt::Password.new(@user["password"]) == params["password"]
-        #add cookie for the user
         session["user_id"] = @user["id"]
         flash["notice"] = "You are now logged in"
         redirect_to "/places"
       else
         flash["notice"] = "Login not recognized. Please try again"
-        redirect_to "/login"
+        redirect_to "/places"
       end
+    else
+      flash["notice"] = "Login not recognized. Please try again"
+      redirect_to "/login"
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -68,5 +68,6 @@ ActiveRecord::Schema[7.1].define(version: 2022_03_09_020701) do
     t.datetime "updated_at", null: false
   end
 
+end
 
 
