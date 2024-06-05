c69366286378d2083db3e63ecf17b0cabd3b3f1c
Samarth Saran <samarth.saran@kellogg.northwestern.edu>
2024-06-05 05:13:38 +0000

    commit 1

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,21 @@ class EntriesController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
+    @entry["user_id"] = session["user_id"]
+    @entry.uploaded_image.attach(params["uploaded_image"])
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
+    else
+    flash["notice"] = "Please login before proceeding"
+    redirect_to "/login"
+    end
   end
 
 end
